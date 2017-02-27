4eeaf48306d2c620e025e8e1a5702e139147ac60
hsw <hsw@hswtekimacbook-air.local>
2017-02-27 20:12:01 +0800

    Initial Commit

---
+++ AutoDial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoDial
+//
+//  Created by hsw on 17/2/27.
+//  Copyright © 2017年 hsw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
